37909fa2b8fba26097f62925c9f8fa3497d8d783
Noemi Diaz <noemi.diazglez@gmail.com>
2025-05-24 17:39:45 +0100

    creating a template for dbconfig file

---
+++ dbconfig_template.py
@@ -0,0 +1,10 @@
+
+# This is a template of dbconfig.py file
+
+mysql = {
+    'host': "localhost",
+    'user': "your_username",
+    'password': "your_password",
+    'database': "your_database"
+}
+
